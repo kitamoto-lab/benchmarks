44cb3abf5747df4e278587764eb0f4053228b70e
Bastien Vuillod <bastien.vuillod@gmail.com>
2023-06-19 10:55:36 +0900

    add train.py in regression, regression is now available

---
+++ analysis/regression/train.py
@@ -0,0 +1,149 @@
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+
+from FrameDatamodule import TyphoonDataModule
+from lightning_resnetReg import LightningResnetReg
+from lightning_VggReg import LightningVggReg
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+import config
+from argparse import ArgumentParser
+
+from datetime import datetime
+import torch
+
+start_time_str = str(datetime.now().strftime("%Y_%m_%d-%H.%M.%S"))
+
+def parse_args(args):
+    args_parsing = ""
+    if args.model_name not in ["resnet18", "resnet50", "vgg"]:
+        args_parsing += "Please give model_name among resnet18, 50 or vgg\n"
+    if args.size not in ["512", "224", 512, 224]:
+        args_parsing += "Please give size equals to 512 or 224\n"
+    if args.cropped not in ["False", "True", False, True]:
+        args_parsing += "Please give cropped equals to False or True\n"
+    if int(args.device) not in range(torch.cuda.device_count()):
+        args_parsing += "Please give a device number in the range (0, %d)\n" %torch.cuda.device_count()
+    if args.labels not in ["wind", "pressure"]:
+        args_parsing += "Please give size equals to wind or pressure\n"
+
+    if args_parsing != "": 
+        print(args_parsing)
+        raise ValueError("Some arguments are not initialized correctly")
+    
+
+    if args.size == '512' or args.size == 512:
+        args.size = (512,512)
+    elif args.size == '224'  or args.size == 224:
+        args.size = (224, 224)
+    
+    if args.cropped == "False": args.cropped = False
+    if args.cropped == "True": args.cropped = True
+
+    if args.device == None:
+        args.device = config.DEVICES
+    else:
+        args.device = [int(args.device)]
+
+    return args
+
+def train(hparam):
+    
+    hparam = parse_args(hparam)
+
+    logger_name = hparam.labels + "_" + hparam.model_name + "_" + str(hparam.size[0])
+    if hparam.cropped: logger_name += "_cropped"
+    else : logger_name += "_no-crop"
+
+    logger = TensorBoardLogger(
+        save_dir="tb_logs",
+        name= logger_name,
+        default_hp_metric=False,
+    )
+
+    logger.log_hyperparams({
+        'start_time': start_time_str,
+        'LEARNING_RATE': config.LEARNING_RATE,
+        'BATCH_SIZE': config.BATCH_SIZE,
+        'NUM_WORKERS': config.NUM_WORKERS,
+        'MAX_EPOCHS': config.MAX_EPOCHS,
+        'WEIGHTS': config.WEIGHTS, 
+        'LABEL' : hparam.labels,
+        'SPLIT_BY': config.SPLIT_BY, 
+        'LOAD_DATA': config.LOAD_DATA, 
+        'DATASET_SPLIT': config.DATASET_SPLIT, 
+        'STANDARDIZE_RANGE': config.STANDARDIZE_RANGE, 
+        'DOWNSAMPLE_SIZE': hparam.size, 
+        'CROPPED': hparam.cropped,
+        'NUM_CLASSES': config.NUM_CLASSES, 
+        'ACCELERATOR': config.ACCELERATOR, 
+        'DEVICES': hparam.device, 
+        'DATA_DIR': config.DATA_DIR, 
+        'LOG_DIR': config.LOG_DIR,
+        'MODEL_NAME': hparam.model_name,
+        })
+
+    # Set up data
+    data_module = TyphoonDataModule(
+        config.DATA_DIR,
+        batch_size=config.BATCH_SIZE,
+        num_workers=config.NUM_WORKERS,
+        labels=hparam.labels,
+        split_by=config.SPLIT_BY,
+        load_data=config.LOAD_DATA,
+        dataset_split=config.DATASET_SPLIT,
+        standardize_range=config.STANDARDIZE_RANGE,
+        downsample_size=hparam.size,
+        cropped=hparam.cropped
+    )
+
+    # Train
+    if hparam.model_name[:6] == "resnet":
+        resnet = LightningResnetReg(
+            learning_rate=config.LEARNING_RATE,
+            weights=config.WEIGHTS,
+            num_classes=config.NUM_CLASSES,
+            model_name = hparam.model_name
+        )
+    else:
+        vgg = LightningVggReg(
+            learning_rate=config.LEARNING_RATE,
+            weights=config.WEIGHTS,
+            num_classes=config.NUM_CLASSES,
+        )
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath= logger.save_dir + '/' + logger.name + '/version_%d/checkpoints/' % logger.version,
+        filename='model_{epoch}',
+        monitor='validation_loss', 
+        verbose=True,
+        every_n_epochs=1,
+        save_top_k = 5
+        )
+
+    trainer = pl.Trainer(
+        logger=logger,
+        accelerator=config.ACCELERATOR,
+        devices=hparam.device,
+        max_epochs=config.MAX_EPOCHS,
+        default_root_dir=config.LOG_DIR,
+        callbacks=[checkpoint_callback]
+    )
+
+
+    if hparam.model_name=="vgg": trainer.fit(vgg, data_module)
+    if hparam.model_name[:6] =="resnet": trainer.fit(resnet, data_module)
+    
+    return "training finished"
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--model_name", default='resnet')
+    parser.add_argument("--size")
+    parser.add_argument("--cropped", default=False)
+    parser.add_argument("--device")
+    parser.add_argument("--labels", default=config.LABELS)
+    args = parser.parse_args()
+
+    print(train(args))
